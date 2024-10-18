a8d38705a1a6a0045e0c90cdaa358420500efc25
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-10-18 15:11:38 +0300

    updare rep

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from '../src/redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/PsychologistsServices">
+      <BrowserRouter basename="/PsychologistsServices_DiplomaVer">
         <GlobalStyles />
         <App />
       </BrowserRouter>
